ad11aee22a60bda781ad6ac05b7349d87818be2a
SATYAJIT NAYAK <satyajitnayak@SATYAJITs-MacBook-Air.local>
2022-06-20 09:38:03 +0530

    Add GetTopRestaurants() code.

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -20,11 +20,27 @@ export const GetFoodAvailability = async (
 
   return res.status(404).json({ message: 'No food available' });
 };
-export const GetTopRestaurants = (
+
+export const GetTopRestaurants = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const pincode = req.params.pincode;
+  const result = await Vendor.find({
+    pincode: pincode,
+    serviceAvailable: false,
+  })
+    .sort({ rating: 'desc' })
+    .limit(10);
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  }
+
+  return res.status(404).json({ message: 'No food available' });
+};
+
 export const GetFoodIn30Min = (
   req: Request,
   res: Response,
