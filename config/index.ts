49615dabf8d0d6bb9e1cbf738ff3172a7b05acdf
SATYAJIT NAYAK <satyajitnayak@SATYAJITs-MacBook-Air.local>
2022-06-26 01:28:13 +0530

    Fixes for loading config from .env.

--- config/index.ts
+++ config/index.ts
@@ -1,2 +1,5 @@
-export const MONGO_URI = process.env.MONGO_URI;
-export const JWT_SECRET = process.env.JWT_SECRET;
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const MONGO_URI = process.env.MONGO_URI!;
+export const JWT_SECRET = process.env.JWT_SECRET!;
--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -41,17 +41,31 @@ export const GetTopRestaurants = async (
   return res.status(404).json({ message: 'No food available' });
 };
 
-export const GetFoodIn30Min = (
+export const GetFoodIn30Min = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const pincode = req.params.pincode;
+  const result = await Vendor.find({
+    pincode: pincode,
+    serviceAvailable: false,
+  })
+    .sort({ rating: 'desc' })
+    .limit(10);
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  }
+
+  return res.status(404).json({ message: 'No food available' });
+};
+export const SearchFoods = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {};
-export const SearchFoods = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {};
-export const RestaurantById = (
+export const RestaurantById = async (
   req: Request,
   res: Response,
   next: NextFunction
